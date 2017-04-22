a594769883b6e9baa111114458138f372feb8a32
trinhphong <nguyenquoctin19961996@gmail.com>
2017-04-23 02:13:04 +0700

    GO HOME

--- resources/views/client/home/home.blade.php
+++ resources/views/client/home/home.blade.php
@@ -1,14 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">HOME</div>
-
-                    <div class="panel-body">
-                        You are logged in as <strong>HOMEPAGE</strong>!
+            <div class="col-md-12" style="height:450px;background: url('https://d1ffx7ull4987f.cloudfront.net/images/courses/hero/115/front-end-foundations-25091a7930fa01a9fc002a2d1e14e043.jpg') center no-repeat; background-size: cover">
+                <div class="col-md-12" style="padding-top: 50px; position: relative">
+                    <div class="col-md-5" style="height:400px; background-color: white;display: flex; align-items: center; border-radius: 3px 3px 0 0;">
+                        <div style="display: block; padding-right: 5em; padding-left: 5em">
+                            <p style="text-transform: uppercase; font-size: 85%; margin-bottom: 0; margin-top: 0; font-weight: 300; line-height: 1.5">HTML/CSS</p>
+                            <h1 style="margin: 0; font-weight: 700; line-height: 1.1; font-size: 300%">Front-end Foundations</h1>
+                            <p style="font-size: 100%;">Learn the fundamentals of HTML and CSS, while building a strong foundation for more advanced front-end development.</p>
+                            <button class="btn btn-primary">Start Course</button>
+                        </div>
                     </div>
                 </div>
             </div>
